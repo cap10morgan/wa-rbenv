c5d423c28e2d09319212bd863c20e64df2a26d56
Łukasz Niemier <lukasz@niemier.pl>
2015-05-24 15:11:31 +0200

    Remove unneded echo

--- rbenv.fish
+++ rbenv.fish
@@ -1,5 +1,4 @@
 function init --on-event init_rbenv
-  echo "Init rbenv"
   setenv PATH "$HOME/.rbenv/bin" "$HOME/.rbenv/shims" $PATH
   setenv RBENV_SHELL fish
   source '/home/hauleth/.rbenv/libexec/../completions/rbenv.fish'
