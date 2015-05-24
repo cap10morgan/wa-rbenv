d4eeffa017db90593cde1e077f0d7067e91dc4f0
Łukasz Niemier <lukasz@niemier.pl>
2015-05-24 15:10:58 +0200

    Fix

--- rbenv.fish
+++ rbenv.fish
@@ -1,9 +1,19 @@
 function init --on-event init_rbenv
   echo "Init rbenv"
-  set -gx PATH $HOME/.rbenv/bin $PATH
-  eval (rbenv init - | psub)
+  setenv PATH "$HOME/.rbenv/bin" "$HOME/.rbenv/shims" $PATH
+  setenv RBENV_SHELL fish
+  source '/home/hauleth/.rbenv/libexec/../completions/rbenv.fish'
+  rbenv rehash 2>/dev/null
 end
 
 function rbenv
-  command rbenv $argv
+  set command $argv[1]
+  set -e argv[1]
+
+  switch "$command"
+  case rehash shell update use
+    eval (rbenv "sh-$command" $argv)
+  case '*'
+    command rbenv "$command" $argv
+  end
 end
