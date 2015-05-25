569fc9e58f6f7c49670aa32bd6d36fdb17ff50dd
Łukasz Niemier <lukasz@niemier.pl>
2015-05-25 08:47:45 +0200

    Fix style

--- rbenv.fish
+++ rbenv.fish
@@ -1,7 +1,7 @@
 function init --on-event init_rbenv
-  setenv PATH "$HOME/.rbenv/bin" "$HOME/.rbenv/shims" $PATH
-  setenv RBENV_SHELL fish
-  source "$HOME/completions/rbenv.fish"
+  set -gx PATH "$HOME/.rbenv/bin" "$HOME/.rbenv/shims" $PATH
+  set -gx RBENV_SHELL fish
+  source "$HOME/.rbenv/completions/rbenv.fish"
   rbenv rehash 2>/dev/null
 end
 
