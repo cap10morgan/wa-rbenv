d52165aaa0b0d02c8caf34d72f3212f4be6bb07d
Łukasz Niemier <lukasz@niemier.pl>
2015-05-26 22:05:00 +0200

    Fix weird psub error

--- rbenv.fish
+++ rbenv.fish
@@ -1,18 +1,7 @@
 function init --on-event init_rbenv
   set -gx PATH "$HOME/.rbenv/bin" "$HOME/.rbenv/shims" $PATH
   set -gx RBENV_SHELL fish
-  source "$HOME/.rbenv/completions/rbenv.fish"
-  rbenv rehash 2>/dev/null
-end
-
-function rbenv
-  set command $argv[1]
-  set -e argv[1]
-
-  switch "$command"
-  case rehash shell update use
-    eval (rbenv "sh-$command" $argv)
-  case '*'
-    command rbenv "$command" $argv
+  if status --is-interactive
+    source (rbenv init - | psub)
   end
 end
