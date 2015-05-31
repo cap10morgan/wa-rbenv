ec5f9bab18ad14c9cf967939ae8c8dd51d9511c0
Jorge Bucaran <jbucaran@me.com>
2015-05-31 23:43:07 +0900

    check if rbenv exists before running script

--- rbenv.fish
+++ rbenv.fish
@@ -1,6 +1,8 @@
 function init --on-event init_rbenv
-  set -x RBENV_SHELL fish
-  if status --is-interactive
-    source (rbenv init - | psub)
+  if available pyenv
+    set -x RBENV_SHELL fish
+    if status --is-interactive
+      source (rbenv init - | psub)
+    end
   end
 end
